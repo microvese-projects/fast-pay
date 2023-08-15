4f711bc8a824095d136a5b5a021c024f62e89c27
Benson <njugunab655@gmail.com>
2023-08-15 15:28:41 +0300

    Get groups and their dealings

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all.where(user_id: params[:user_id])
+    @groups = Group.all.where(user_id: params[:user_id]).includes(:dealings)
   end
 
   # GET /groups/1 or /groups/1.json

94a606b89286c1cd260973ef576ad2420867f258
Benson <njugunab655@gmail.com>
2023-08-15 09:42:47 +0300

    Limit access to app to logged in users

--- app/controllers/dealings_controller.rb
+++ app/controllers/dealings_controller.rb
@@ -1,4 +1,5 @@
 class DealingsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_dealing, only: %i[show edit update destroy]
 
   # GET /dealings or /dealings.json

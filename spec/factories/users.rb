e533ff4f85a7fdea03d96d81cc38021093c4f14a
Benson <njugunab655@gmail.com>
2023-08-16 17:36:57 +0300

    Add group model spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
-    first_name { 'John' }
-    last_name { 'Doe' }
-    email { 'dummy@gmail.com' }
-    password { '12345678' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.unique.email }
+    password { '1234567' }
+    confirmed_at { Date.today }
   end
 end

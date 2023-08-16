73797d86cc4dcd1fba20016909ee2db8bab88d64
Benson <njugunab655@gmail.com>
2023-08-16 16:48:05 +0300

    Modify the factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { 'MyString' }
+    first_name { 'John' }
+    last_name { 'Doe' }
+    email { 'dummy@gmail.com' }
+    password { '12345678' }
   end
 end

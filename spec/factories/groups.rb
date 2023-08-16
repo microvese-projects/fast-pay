368c199289b627cc808b8d5bfd1ea3febbb0b3de
Benson <njugunab655@gmail.com>
2023-08-16 18:45:59 +0300

    Add real icon image

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :group do
     name { 'Food' }
-    icon { 'icon.png' }
+    icon { 'https://assets.materialup.com/uploads/a4d861ba-07a4-426d-b3cb-67f0353de4e1/preview.jpg' }
   end
 end
